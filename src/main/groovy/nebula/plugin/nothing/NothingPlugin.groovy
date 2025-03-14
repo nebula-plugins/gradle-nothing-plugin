3df2b83dd31b8de38c769e78d6ebfb9d51d0947e
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2025-03-14 12:55:53 -0700

    Add a nothing settings plugin?

--- src/main/groovy/nebula/plugin/nothing/NothingPlugin.groovy
+++ src/main/groovy/nebula/plugin/nothing/NothingPlugin.groovy
@@ -1,12 +1,16 @@
 package nebula.plugin.nothing
 
 import com.google.common.collect.ImmutableList
-import org.apache.commons.logging.Log
+import nebula.plugin.responsible.FacetDefinition
+import nebula.plugin.responsible.TestFacetDefinition
+import nebula.plugin.responsible.NebulaFacetPlugin
 import org.apache.commons.logging.impl.NoOpLog
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
+import org.gradle.api.plugins.JavaPlugin
 
 /**
  * Does nothing.
@@ -22,5 +26,15 @@ class NothingPlugin extends NoOpLog implements Plugin<Project> {
         this.project = project
 
         logger.info("Hello, " + ImmutableList.of("friend").get(0));
+
+        project.plugins.withType(JavaPlugin).configureEach {
+            project.getPlugins().apply(NebulaFacetPlugin)
+            NamedDomainObjectContainer<FacetDefinition> facetDefinitions = (NamedDomainObjectContainer<FacetDefinition>) project.getExtensions().getByName("facets")
+            TestFacetDefinition smokeTestFacet = new TestFacetDefinition("smokeTest")
+            smokeTestFacet.setTestTaskName("smokeTest")
+            smokeTestFacet.setIncludeInCheckLifecycle(true)
+            smokeTestFacet.setParentSourceSet("test")
+            facetDefinitions.add(smokeTestFacet)
+        }
     }
 }
