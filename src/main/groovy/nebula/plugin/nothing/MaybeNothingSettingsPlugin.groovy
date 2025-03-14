1d1ac1590c837beba5e0d66b67bd1577cbc22423
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2025-03-14 13:00:01 -0700

    Add a nothing settings plugin?

--- src/main/groovy/nebula/plugin/nothing/MaybeNothingSettingsPlugin.groovy
+++ src/main/groovy/nebula/plugin/nothing/MaybeNothingSettingsPlugin.groovy
@@ -2,18 +2,15 @@ package nebula.plugin.nothing
 
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
-import org.gradle.api.plugins.JavaPlugin
 
 class MaybeNothingSettingsPlugin implements Plugin<Settings> {
     @Override
     void apply(Settings settings) {
         settings.gradle.rootProject {rootProject ->
-            rootProject.plugins.withType(JavaPlugin).configureEach {
-                def dependencyHandler = rootProject.buildscript.dependencies
-                def buildClasspathDependencies = ['com.netflix.nebula:nebula-project-plugin:11.0.0']
-                buildClasspathDependencies.each {
-                    dependencyHandler.add("classpath", it)
-                }
+            def dependencyHandler = rootProject.buildscript.dependencies
+            def buildClasspathDependencies = ['com.netflix.nebula:nebula-project-plugin:11.0.0']
+            buildClasspathDependencies.each {
+                dependencyHandler.add("classpath", it)
             }
         }
     }
