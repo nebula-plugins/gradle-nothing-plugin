c68b310e0993f155122bac90e377e577807d6be3
John Burns <wakingrufus@gmail.com>
2025-12-08 10:33:52 -0600

    attach publish action to Publish environment
    update dependencies
    fix archRules failures

--- nebula-nothing-library/build.gradle.kts
+++ nebula-nothing-library/build.gradle.kts
@@ -7,3 +7,7 @@ description = "A Nebula library that does nothing"
 dependencyLocking {
     lockAllConfigurations()
 }
+
+dependencies {
+    implementation("org.jspecify:jspecify:1.0.0")
+}
--- nebula-nothing-library/src/main/java/com/netflix/nebula/nothing/NothingClass.java
+++ nebula-nothing-library/src/main/java/com/netflix/nebula/nothing/NothingClass.java
@@ -1,7 +1,10 @@
 package com.netflix.nebula.nothing;
 
+import org.jspecify.annotations.NullMarked;
+
 /**
  * Does nothing
  */
+@NullMarked
 public class NothingClass {
 }
